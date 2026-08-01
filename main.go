28db864892d2eac814a98bfc333f82b9a262d68b
Erik Martino <erik.martino@gmail.com>
2026-08-01 17:30:36 +0200

    Update main.go default host binding to 0.0.0.0 and support PUBLIC_URL

--- main.go
+++ main.go
@@ -16,18 +16,27 @@ func main() {
 		defaultPort = "8080"
 	}
 
+	defaultHost := os.Getenv("HOST")
+	if defaultHost == "" {
+		defaultHost = "0.0.0.0"
+	}
+
+	publicURL := os.Getenv("PUBLIC_URL")
+
 	port := flag.String("port", defaultPort, "HTTP server listening port")
-	host := flag.String("host", "localhost", "HTTP server listening host")
+	host := flag.String("host", defaultHost, "HTTP server listening host")
 	flag.Parse()
 
 	addr := fmt.Sprintf("%s:%s", *host, *port)
-	baseURL := fmt.Sprintf("http://%s", addr)
+	if publicURL == "" {
+		publicURL = fmt.Sprintf("http://%s", addr)
+	}
 
-	session := jmap.DefaultSession(baseURL)
+	session := jmap.DefaultSession(publicURL)
 	server := jmap.NewServer(session)
 
-	log.Printf("Starting JMAP server on http://%s", addr)
-	log.Printf("Discovery endpoint: %s/.well-known/jmap", baseURL)
+	log.Printf("Starting JMAP server on http://%s (public URL: %s)", addr, publicURL)
+	log.Printf("Discovery endpoint: %s/.well-known/jmap", publicURL)
 
 	if err := http.ListenAndServe(addr, server.Handler()); err != nil {
 		log.Fatalf("Server failed: %v", err)
