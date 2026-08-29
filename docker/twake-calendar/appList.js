8115682693def0ca7968a9d5b314988d020cb3cd
Erik Martino <erik.martino@gmail.com>
2026-08-29 19:18:57 +0200

    feat(twake-calendar): mount external appList.js in docker-compose

---
+++ docker/twake-calendar/appList.js
@@ -0,0 +1,17 @@
+var appList = [
+  {
+    name: 'Mail',
+    link: 'http://localhost:3333',
+    icon: '/assets/images/svg/app-mail.svg'
+  },
+  {
+    name: 'Drive',
+    link: 'http://localhost:8088',
+    icon: '/assets/images/svg/app-drive.svg'
+  },
+  {
+    name: 'Webmail',
+    link: 'http://localhost:3000',
+    icon: '/assets/images/svg/app-mail.svg'
+  }
+];
